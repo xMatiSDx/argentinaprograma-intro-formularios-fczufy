9b4701eb73244ebe55b2c34eaf5d1221fc2db379
xMatiSDx <86577536+xMatiSDx@users.noreply.github.com>
2022-09-15 00:57:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# argentinaprograma-intro-formularios-fczufy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/argentinaprograma-intro-formularios-fczufy)
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+// importamos las librerias de formulario que vamos a necesitar
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  isDisabled = true;
+  form: FormGroup;
+
+  // Inyectar en el constructor el formBuilder
+  constructor(private formBuilder: FormBuilder){ 
+    ///Creamos el grupo de controles para el formulario de login
+    this.form= this.formBuilder.group({
+      password:['',[Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email]],
+      username:['', [Validators.required, Validators.minLength(5), Validators.maxLength(12)]],
+   })
+  }
+
+  ngOnInit() {}
+
+  get Password(){
+    return this.form.get("password");
+  }
+ 
+  get Mail(){
+   return this.form.get("email");
+  }
+
+  get Username() {
+    return this.form.get("username");
+  }
+
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return false
+  }
+
+  get UsernameValid(){
+    return this.Username?.touched && !this.Username?.valid;
+  }
+
+ 
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+ 
+  }
+
+
+}
